69413a45af1f881ebf3d0ab0ced09749ab099e87
Shuhuai Cao <csh980717@outlook.com>
2022-07-10 20:23:01 -0400

    Main activity does nothing

--- app/src/main/java/tech/tennoji/dronewatch/MainActivity.kt
+++ app/src/main/java/tech/tennoji/dronewatch/MainActivity.kt
@@ -1,31 +1,11 @@
 package tech.tennoji.dronewatch
 
-import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
-
-            // Get new FCM registration token
-            val token = task.result
-
-            // Log and toast
-//            val msg = getString(R.string.msg_token_fmt, token)
-            val msg = token
-            Log.d("token", msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        })
     }
 }
