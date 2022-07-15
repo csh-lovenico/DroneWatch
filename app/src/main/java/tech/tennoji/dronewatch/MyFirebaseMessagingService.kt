165c8c4888b0bdc6f97dd83a90e9bd3f0a608e70
Shuhuai Cao <csh980717@outlook.com>
2022-07-15 15:29:18 -0400

    Fix log tag

--- app/src/main/java/tech/tennoji/dronewatch/MyFirebaseMessagingService.kt
+++ app/src/main/java/tech/tennoji/dronewatch/MyFirebaseMessagingService.kt
@@ -1,6 +1,5 @@
 package tech.tennoji.dronewatch
 
-import android.content.ContentValues.TAG
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -8,7 +7,7 @@ import com.google.firebase.messaging.RemoteMessage
 class MyFirebaseMessagingService: FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Log.d(TAG, "Refreshed token: $token")
+        Log.d(this.javaClass.toString(), "Refreshed token: $token")
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
