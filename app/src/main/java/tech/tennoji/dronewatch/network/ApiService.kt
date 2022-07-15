931a4a85676076c4c5019df2f1a801c0cef2bae6
Shuhuai Cao <csh980717@outlook.com>
2022-07-14 20:16:50 -0400

    Change method annotations

--- app/src/main/java/tech/tennoji/dronewatch/network/ApiService.kt
+++ app/src/main/java/tech/tennoji/dronewatch/network/ApiService.kt
@@ -6,6 +6,7 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -28,27 +29,27 @@ interface ApiService {
 
     @POST("/subscribe")
     fun subscribeToTopicAsync(
-        @Query("token") token: String,
-        @Query("area") area: String
+        @Field("token") token: String,
+        @Field("area") area: String
     ): Deferred<WebMessage<Int>>
 
     @POST("/unsubscribe")
     fun unsubscribeToTopicAsync(
-        @Query("token") token: String,
-        @Query("area") area: String
+        @Field("token") token: String,
+        @Field("area") area: String
     ): Deferred<WebMessage<Int>>
 
     @GET("/getLatestRecord")
     fun getLatestDroneRecordAsync(@Query("droneId") droneId: String): Deferred<WebMessage<DroneRecord>>
 
     @GET("/getSubscribedAreas")
-    fun getSubscribedAreasAsync(@Query("token") token: String): Deferred<List<String>>
+    fun getSubscribedAreasAsync(@Field("token") token: String): Deferred<WebMessage<List<String>>>
 
     @GET("/getNotSubscribedAreas")
-    fun getNotSubscribedAreasAsync(@Query("token") token: String): Deferred<List<String>>
+    fun getNotSubscribedAreasAsync(@Field("token") token: String): Deferred<WebMessage<List<String>>>
 
     @GET("/getSubscribedAreaStatus")
-    fun getSubscribedAreaStatusAsync(@Query("token") token: String): Deferred<List<FenceStatus>>
+    fun getSubscribedAreaStatusAsync(@Field("token") token: String): Deferred<WebMessage<List<FenceStatus>>>
 
 }
 
