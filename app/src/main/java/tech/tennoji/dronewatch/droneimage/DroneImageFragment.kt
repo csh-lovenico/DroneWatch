cc511071c2c508c5b06716b491ba6a8a10df70a0
Shuhuai Cao <csh980717@outlook.com>
2022-07-16 23:18:46 -0400

    DroneImageFragment

--- app/src/main/java/tech/tennoji/dronewatch/droneimage/DroneImageFragment.kt
+++ app/src/main/java/tech/tennoji/dronewatch/droneimage/DroneImageFragment.kt
@@ -1,12 +1,21 @@
 package tech.tennoji.dronewatch.droneimage
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
+import coil.load
 import tech.tennoji.dronewatch.R
+import tech.tennoji.dronewatch.network.BASE_URL
+import java.time.format.DateTimeFormatter
 
 class DroneImageFragment : Fragment() {
 
@@ -24,8 +33,68 @@ class DroneImageFragment : Fragment() {
     }
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[DroneImageViewModel::class.java]
+        val droneId = arguments?.getString("droneId")
+        if (droneId != null) {
+            viewModel.setDroneId(droneId)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this)[DroneImageViewModel::class.java]
+
+        viewModel.droneId.observe(viewLifecycleOwner) {
+            it?.let {
+                viewModel.launch()
+            }
+        }
+
+        view.findViewById<Button>(R.id.pause_button).setOnClickListener {
+            Log.i("Coroutine Test", "Stop button")
+            viewModel.end()
+        }
+
+        view.findViewById<Button>(R.id.resume_button).setOnClickListener {
+            viewModel.launch()
+        }
+
+        viewModel.record.observe(viewLifecycleOwner) {
+            it?.let {
+                val imageUrl = BASE_URL + "/files/" + it.imagePath
+                view.findViewById<ImageView>(R.id.drone_image).load(imageUrl)
+                view.findViewById<TextView>(R.id.drone_id_text).text =
+                    "Drone ID: " + it.metadata.droneId
+                view.findViewById<TextView>(R.id.last_update_text).text =
+                    "Last update: " + it.timestamp.format(
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                    )
+            }
+        }
+
+        viewModel.isPaused.observe(viewLifecycleOwner) {
+            it?.let {
+                if (it) {
+                    view.findViewById<Button>(R.id.pause_button).isEnabled = false
+                    view.findViewById<Button>(R.id.resume_button).isEnabled = true
+                } else {
+                    view.findViewById<Button>(R.id.pause_button).isEnabled = true
+                    view.findViewById<Button>(R.id.resume_button).isEnabled = false
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onPause(owner: LifecycleOwner) {
+                super.onPause(owner)
+                viewModel.end()
+            }
+
+            override fun onDestroy(owner: LifecycleOwner) {
+                super.onDestroy(owner)
+                viewModel.end()
+            }
+        })
     }
 }
--- app/src/main/java/tech/tennoji/dronewatch/droneimage/DroneImageViewModel.kt
+++ app/src/main/java/tech/tennoji/dronewatch/droneimage/DroneImageViewModel.kt
@@ -1,10 +1,67 @@
 package tech.tennoji.dronewatch.droneimage
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import tech.tennoji.dronewatch.network.Api
+import tech.tennoji.dronewatch.network.DroneRecord
 
 class DroneImageViewModel : ViewModel() {
 
+    private var viewModelJob = SupervisorJob()
+
+    private val coroutineScope = CoroutineScope(
+        viewModelJob + Dispatchers.IO
+    )
+
+    private var active = false
+
+    private val _record = MutableLiveData<DroneRecord>()
+    val record: LiveData<DroneRecord>
+        get() = _record
+
+    private val _droneId = MutableLiveData<String>()
+    val droneId: LiveData<String>
+        get() = _droneId
+
+    private val _isPaused = MutableLiveData<Boolean>()
+    val isPaused: LiveData<Boolean>
+        get() = _isPaused
 
 
+    fun setDroneId(droneId: String) {
+        _droneId.value = droneId
+    }
+
+    private fun refreshData() {
+        coroutineScope.launch {
+            while (active) {
+                val result =
+                    droneId.value?.let { Api.retrofitService.getLatestDroneRecordAsync(it).await() }
+                if (result != null) {
+                    if (result.code == 200) {
+                        result.data?.let {
+                            withContext(Dispatchers.Main) {
+                                _record.value = result.data
+                            }
+                        }
+                    }
+                }
+                delay(1000)
+            }
+        }
+    }
+
+    fun launch() {
+        active = true
+        _isPaused.value = false
+        refreshData()
+    }
+
+    fun end() {
+        active = false
+        _isPaused.value = true
+    }
 
 }
